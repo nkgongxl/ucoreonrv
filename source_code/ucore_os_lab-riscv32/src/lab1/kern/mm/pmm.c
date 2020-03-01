670a723d03fc12ed3a62f4bb76de067ea7ac22b5
KeLee <kelee5453@outlook.com>
2020-03-01 13:29:34 +0800

    整理

--- source_code/ucore_os_lab-riscv32/src/lab1/kern/mm/pmm.c
+++
@@ -1,6 +0,0 @@
-#include <pmm.h>
-
-/**
- * @brief      Do nothing here in lab 1
- */
-void pmm_init(void) {}
--- source_code/ucore_os_lab-riscv32/src/lab8/kern/driver/intr.c
+++
@@ -1,8 +0,0 @@
-#include <intr.h>
-#include <riscv.h>
-
-/* intr_enable - enable irq interrupt */
-void intr_enable(void) { set_csr(sstatus, SSTATUS_SIE); }
-
-/* intr_disable - disable irq interrupt */
-void intr_disable(void) { clear_csr(sstatus, SSTATUS_SIE); }
